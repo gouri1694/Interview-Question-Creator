3fc46995150990cf79a7530947998d54f4d52685
gouri1694 <igouri8@gmail.com>
2025-09-03 14:42:58 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author='Gouri S Patil',
+    author_email='igouri8@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
